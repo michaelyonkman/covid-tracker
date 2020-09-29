0f1a5e34905a38c62ff3da4adfd60f2f2e1a3997
Michael Yonkman <michaelyonkman@gmail.com>
2020-09-29 12:50:46 -0500

    StateData component added and rendering new positive cases

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import StateData from './components/StateData';
 
 function App() {
+  const [selectState, setSelectState] = useState('mi');
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Covid-19 Tracker</h1>
-        <StateData />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <select onChange={(e) => setSelectState(e.target.value)}>
+        <option value="mi">Michigan</option>
+        <option value="ca">California</option>
+      </select>
+      <StateData selectState={selectState} />
     </div>
   );
 }
--- src/components/StateData.js
+++ src/components/StateData.js
@@ -1,30 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import { VictoryBar, VictoryChart, VictoryTheme } from 'victory';
 
-const StateData = () => {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [data, setData] = useState({});
+const StateData = (props) => {
+  const [data, setData] = useState([]);
 
   const doFetch = async (state) => {
     let response = await fetch(
-      `https://api.covidtracking.com/v1/states/${state}/current.json`
+      `https://api.covidtracking.com/v1/states/${state}/daily.json`
     );
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     } else {
       let json = await response.json();
-      setIsLoaded(true);
-      setData(json);
-      console.log(json);
+      const formattedData = json.slice(0, 7).map((day) => {
+        const container = {};
+        container.date = new Date(day.lastUpdateEt);
+        container.positiveIncrease = day.positiveIncrease;
+        return container;
+      });
+      setData(formattedData);
     }
   };
 
   useEffect(() => {
-    doFetch('ca');
-  }, []);
-  if (!isLoaded) {
+    doFetch(props.selectState);
+  }, [props.selectState]);
+
+  if (!data.length) {
     return <div>Loading...</div>;
   } else {
-    return <div>{data.death}</div>;
+    return (
+      <Fragment>
+        <h1>New Cases</h1>
+        <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
+          <VictoryBar data={data} x="date" y="positiveIncrease" />
+        </VictoryChart>
+      </Fragment>
+    );
   }
 };
 
